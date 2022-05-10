ba5e54994535cd1ba9b5a4742601fac408a078a0
Nguyễn <ducduy10k@gmail.com>
2022-05-10 17:41:26 +0700

    2.10

--- pages/posts/[postid].tsx
+++ pages/posts/[postid].tsx
@@ -1,45 +1,49 @@
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
-import { useRouter } from 'next/router';
-import  React from 'react';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+import { useRouter } from 'next/router'
+import React from 'react'
 
 export interface PostPageProps {
-  posts: []
+  post: any
 }
 
-export default function PostPage(props: PostPageProps) {
-  const router= useRouter();
+export default function PostDetailPage({post}: PostPageProps) {
+  const router = useRouter()
+  console.log(post)
+  if(!post) return null
   return (
     <div>
-      Post page : 
-      <p>Query : {JSON.stringify(router.query)}</p>
-      <ul>
-        {props.posts.map((post:any) => (<li key={post.id}>{post.title}</li>))}
-      </ul>
+      Post page :<p>Query : {JSON.stringify(post)}</p>
     </div>
-  );
+  )
 }
 
-
-
-export const getStaticProps: GetStaticProps<PostPageProps> = async (context: GetStaticPropsContext) => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+export const getStaticPaths: GetStaticPaths = async () => {
+  console.log('Get paths');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
   const data = await response.json();
+  // console.log(data)
+  var arr = data.map((post:any)=>({params:{postid:post.id + ''}}));
+  return {
+    paths: arr,
+    fallback: false // true or 'blocking'
+  };
+}
+
+export const getStaticProps: GetStaticProps<PostPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log('Get props');
+  const postId = context.params?.postid;
+  console.log(postId)
+
+  if(!postId) return {notFound: true}
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  const data = await response.json()
   console.log(data)
   return {
     props: {
-      posts: data.map((x: any)=>({
-        id: x.id, 
-        title : x.title
-      }))
+      post: data,
     },
-
   }
 }
 
-export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-
-  return {
-      paths: [], //indicates that no page needs be created at build time
-      fallback: 'blocking' //indicates the type of fallback
-  }
-}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,36 +1,41 @@
-import { GetStaticProps, GetStaticPropsContext } from 'next';
-import { useRouter } from 'next/router';
-import * as React from 'react';
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import * as React from 'react'
 
 export interface PostListPageProps {
   posts: []
-
 }
 
-export default function PostListPage (props: PostListPageProps) {
-    const router= useRouter();
-    return (
-      <div>
-        Post page : 
-        <p>Query : {JSON.stringify(router.query)}</p>
-        <ul>
-          {props.posts.map((post:any) => (<li key={post.id}>{post.title}</li>))}
-        </ul>
-      </div>
-    );
+export default function PostListPage(props: PostListPageProps) {
+  const router = useRouter()
+  return (
+    <div>
+      Post page :<p>Query : {JSON.stringify(router.query)}</p>
+      <ul>
+        {props.posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-
-export const getStaticProps: GetStaticProps<PostListPageProps> = async (context: GetStaticPropsContext) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    console.log(data)
-    return {
-      props: {
-        posts: data.map((x: any)=>({
-          id: x.id, 
-          title : x.title
-        }))
-      },
-    }
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+  return {
+    props: {
+      posts: data.map((x: any) => ({
+        id: x.id,
+        title: x.title,
+      })),
+    },
   }
+}
